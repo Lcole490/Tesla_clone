797c5d27f741e56a4345cb33b230c9a1052688b0
Lcole.engr <lcole.engr@gmail.com>
2021-05-25 10:51:07 -0400

    added menu items with null links to header.js

--- tesla-clone/src/Components/Header.js
+++ tesla-clone/src/Components/Header.js
@@ -6,6 +6,19 @@ function Header() {
         <Container>
             <Logo src="/images/tesla_logo.png"/>
 
+            <Menu>
+                    <a href= "#"> Model S</a>
+                    <a href= "#"> Model 3</a>
+                    <a href= "#"> Model X</a>
+                    <a href= "#"> Model Y</a>
+                    
+            </Menu>
+
+            <RightMenu>
+                     <a href = "#">Shop</a>
+                     <a href = "#">Tesla Account</a>
+            </RightMenu>
+
             
         </Container>
     )
@@ -17,7 +30,46 @@ export default Header
 
 
 
-const Container = styled.div ``
+const Container = styled.div `
 
 
-const Logo = styled.img ``
+min-height: 60px;
+position: fixed;
+display: flex;
+align-items: center;
+padding: 0 20px;
+top: 0;
+left: 0;
+right: 0;
+`
+
+
+const Logo = styled.img `
+
+height: 100px;
+`
+
+const Menu = styled.div `
+
+display: flex;
+align-items: center;
+justify-content: center;
+flex: 1;
+
+a{
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+}
+`
+
+const RightMenu = styled.div `
+
+a{
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right:  10px;
+    flex-wrap: no-wrap;
+}
+`
